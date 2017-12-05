80c669c6cd5af7d80a01f086166a599efbce22f2
Nonzulu <knzulu16@gmail.com>
2017-12-05 09:31:01 +0200

    changed file in procfile file

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var app = express();
 var JsonParser = require("body-parser");
 var plumber = require("./models");
-var ObjectId= require('mongodb').ObjectId;
+var ObjectId = require('mongodb').ObjectId;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -22,94 +22,93 @@ app.use(logger("dev"));
 
 
 
-app.post("/api/plumbers/slots/:slot/days/:day", function(req, res,next) {
+app.post("/api/plumbers/slots/:slot/days/:day", function(req, res, next) {
 
-var slot=req.params.slots;
-var days=req.params.days;
-console.log("@@@@",days);
-plumber.find({
+  var slot = req.params.slots;
+  var days = req.params.days;
+  console.log("@@@@", days);
+  plumber.find({
 
-  Slots:slot,
-  days:days
-},function(err,results){
-  console.log(results);
-if(err){
-  return err;
-}
-else{
-  res.json({
-    status:"success",
-    reslt:results
+    Slots: slot,
+    days: days
+  }, function(err, results) {
+    console.log(results);
+    if (err) {
+      return err;
+    } else {
+      res.json({
+        status: "success",
+        reslt: results
+      })
+    }
   })
-}
-})
 })
 
 
 
 
 app.get("/api/plumbers/:id", function(req, res) {
-  var id =req.params.id;
+  var id = req.params.id;
   console.log(id);
   plumber.findOne({
-   _id:ObjectId(id)
- },function(err,reslt){
-     if(err){
+    _id: ObjectId(id)
+  }, function(err, reslt) {
+    if (err) {
 
-       return res.json({
-         status:"error",
-         error:err,
-         results:[]
-       })
-     }
-     else{
-       res.json({
-         results:reslt,
-         status:"success"
-       })
-     }
-   })
- })
-
-
- app.post("/api/plumbers", function(req, res) {
-   var username = req.body.username;
-   var email = req.body.email;
-   var contact_Number =req.body.contact_Number;
-   var days=req.body.days;
-   var Slots=req.body.Slots;
-   plumber.create({
-     username:username,
-     email:email,
-     contact_Number:contact_Number,
-     days:days,
-     Slots:Slots
-   },function(err,results){
-     if(err)  {
-       console.log(err);
-     }
-        else {
-         res.json({results});
-       }
-
- });
- })
+      return res.json({
+        status: "error",
+        error: err,
+        results: []
+      })
+    } else {
+      res.json({
+        results: reslt,
+        status: "success"
+      })
+    }
+  })
+})
 
 
+app.post("/api/plumbers", function(req, res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var contact_Number = req.body.contact_Number;
+  var days = req.body.days;
+  var Slots = req.body.Slots;
+  plumber.create({
+    username: username,
+    email: email,
+    contact_Number: contact_Number,
+    days: days,
+    Slots: Slots
+  }, function(err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json({
+        results
+      });
+    }
 
- app.get("/api/plumbers", function(req, res) {
+  });
+})
 
-   plumber.find({
-},function(err,data){
-     if(err)  {
-       console.log(err);
-     }
-        else {
-         res.json({data});
-       }
 
- });
- })
+
+app.get("/api/plumbers", function(req, res) {
+
+  plumber.find({}, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json({
+        data
+      });
+    }
+
+  });
+})
 
 
 
